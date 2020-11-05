0d09afe76d5deedf04d2db592f6b5f57e66c6a93
saitama93 <igalilmi32@gmail.com>
2020-11-05 10:08:08 +0100

    Mise en place de @IsGranted('ROLE_USER') pour la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="Home.index")
+     * @IsGranted("ROLE_USER"))
      */
     public function index()
     {
