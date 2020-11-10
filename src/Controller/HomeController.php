80ccdb39331d7bde2c067ed6bf80bac3c867644d
saitama93 <igalilmi32@gmail.com>
2020-11-10 09:01:02 +0000

    Redirection après connection en fonction du rôle

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
@@ -12,10 +13,24 @@ class HomeController extends AbstractController
      * @Route("/", name="Home.index")
      * @IsGranted("ROLE_USER"))
      */
-    public function index()
+    public function index(AuthenticationUtils $authenticationUtils)
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        if ($this->getUser()) {
+            $roles = $this->getUser()->getRoles();
+
+            if (($this->container->get('security.authorization_checker')->isGranted('ROLE_ADMIN'))
+                && ($this->container->get('security.authorization_checker')->isGranted('ROLE_USER'))) {
+                return $this->render('admin/home/index.html.twig', [
+                    'role' => $roles
+                ]);
+            } elseif ($this->container->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+                return $this->render('home/index.html.twig', [
+                    'role' => $roles
+                ]);
+            }
+        }
+
+
+        return $this->render('home/index.html.twig');
     }
 }
